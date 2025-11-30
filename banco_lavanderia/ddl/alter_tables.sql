0799a074f413cc614be49c528d4ed7835ab65044
André Neves <andrenevesxavier276@gmail.com>
2025-11-30 20:08:44 -0300

    scrips para fazer alteraõões nos dados

--- banco_lavanderia/ddl/alter_tables.sql
+++ banco_lavanderia/ddl/alter_tables.sql
@@ -23,3 +23,10 @@ ALTER TABLE inclui ADD CONSTRAINT fk_inclui_pedido FOREIGN KEY (ID_servico) REFE
 ALTER TABLE telefone MODIFY ID_telefone INT NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE pedido MODIFY valor_total DECIMAL(10, 2) NULL;
+
+ALTER TABLE pedido DROP FOREIGN KEY pedido_ibfk_1;
+ALTER TABLE telefone DROP FOREIGN KEY telefone_ibfk_1;
+
+ALTER TABLE pedido ADD CONSTRAINT fk_pedido_cliente FOREIGN KEY (CPF_cliente) REFERENCES clientes(CPF);
+
+ALTER TABLE telefone ADD CONSTRAINT fk_telefone_cliente FOREIGN KEY (CPF_cliente) REFERENCES clientes(CPF);
